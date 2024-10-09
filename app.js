e8afc1c09cd7cd602da7a805cf63210ca9c1c4c8
Muhammad Hasnain <hasnain0893@gmail.com>
2024-10-09 20:45:50 +0500

    winner

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
 let userScore = 0
 let computerScore = 0
 
+let msg = document.getElementById('msg')
+
 const choices = document.querySelectorAll('.choice')
+choices.forEach((choice) => {
+    choice.addEventListener('click', () => {
+        const userChoice = choice.getAttribute('id')
+        playGame(userChoice)
+    })
+})
 const generateComputerChoice = () => {
     const options = ['rock', 'paper', 'scissors']
     const ranind = Math.floor(Math.random() * 3)
     return options[ranind]
 }
 const drawGame = () => {
+    msg.innerText = 'Game draw'
+    msg.style.backgroundColor = '#081b31'
     console.log('Game draw');
+
+}
+const showWinner = (userWin) => {
+    if (userWin) {
+        msg.innerText = 'You Win'
+        msg.style.backgroundColor = 'green'
+        console.log('You win');
+    } else {
+        msg.innerText = 'You lose'
+        msg.style.backgroundColor = 'red'
+
+    }
 
 }
 const playGame = (userChoice) => {
     console.log('user choice = ', userChoice);
 
-    // generete computer choice
-    const computerChioce = generateComputerChoice()
-    console.log('computer choice = ', computerChioce);
+    // generate computer choice
+    const computerChoice = generateComputerChoice()
+    console.log('computer choice = ', computerChoice);
 
-    if (userChoice === computerChioce) {
+    if (userChoice === computerChoice) {
         drawGame()
     } else {
-        let userWin = true
-        if (userChoice === 'rock') {
-            userWin = userChoice === 'paper' ? false : true
+        let userWin = false
+
+        // Define user win conditions
+        if (
+            (userChoice === 'rock' && computerChoice === 'scissors') ||
+            (userChoice === 'paper' && computerChoice === 'rock') ||
+            (userChoice === 'scissors' && computerChoice === 'paper')
+        ) {
+            userWin = true
         }
 
+        showWinner(userWin)
     }
-    choices.forEach((choice) => {
-        choice.addEventListener('click', () => {
-            const userChoice = choice.getAttribute('id')
-            playGame(userChoice)
-        })
-    })
 }
